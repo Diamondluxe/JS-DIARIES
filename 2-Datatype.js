4c208bc0d3b5b71f578e31bb2874d938be058081
Diamondluxe <hehediamondluxe@gmail.com>
2026-07-05 17:27:36 +0500

    Learning operators

---
+++ 2-Datatype.js
@@ -0,0 +1,22 @@
+let string = "Javascript";
+let number = 10;
+let boolean = true;
+let float = 3.14;
+let marks;
+let phone = null;
+let fruits = ["apple", "banana", "mango", "grapes"];
+let person = {
+    name: "Dua Sheikh",
+    age: 18,
+    gpa: 3.95,
+    course: "Javascript"
+};
+
+console.log(string);
+console.log(number);
+console.log(boolean);
+console.log(float);
+console.log(marks);
+console.log(phone);
+console.log(fruits);
+console.log(person.name);
---
+++ 3-Arithmetic.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+let count = 5;
+count++;
+console.log(count);
+
+count--;
+console.log(count);
