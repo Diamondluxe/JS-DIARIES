34d29d16c5bc5d11daee318125e944121ce2d2fd
Diamondluxe <hehediamondluxe@gmail.com>
2026-07-05 19:22:09 +0500

    Prompt files added

---
+++ 7-Ternary.js
@@ -0,0 +1,7 @@
+let age = 20;
+let result = age > 18 ? "Adult" : "Not Adult";
+console.log(result);
+
+let num = 5;
+let output = num % 2 == 0 ? "Even" : "Odd";
+console.log(output);
---
+++ 8-Input.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+let name = prompt("Enter your name: ");
+let age = prompt("Enter your age: ");
+console.log(name);
+console.log(age);
