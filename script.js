a9a5674ccb5a62dd6c792f9842d9c6917e57ae3a
Diamondluxe <hehediamondluxe@gmail.com>
2026-07-18 17:44:37 +0500

    DOM started

---
+++ script.js
@@ -0,0 +1,8 @@
+let heading = document.getElementById("heading"); //only works for id, not class or tag
+console.log(heading);
+
+let para = document.querySelector(".text");  // updated version can select id, class, tag, etc
+
+
+document.getElementById("demo").innerHTML = "Hello This is a Dua Sheikh."; //changes the content of the element with id "demo"
+
---
+++ script1.js
@@ -0,0 +1,16 @@
+
+document.getElementById("Box").style.color = "pink"; //changes the color of the text of the element with id "Box"
+document.getElementById("Box").style.fontSize = "50px"; //changes the font size of the text of the element with id "Box"
+document.getElementById("Box").style.background = "black"; //changes the background color of the element with id "demo"
+
+document.getElementById("Box1").style.color = "black";
+document.getElementById("Box1").style.fontSize = "50px";
+document.getElementById("Box1").style.background = "pink";
+
+document.getElementById("Box2").style.color = "pink";
+document.getElementById("Box2").style.fontSize = "50px";
+document.getElementById("Box2").style.background = "black";
+
+document.getElementById("Box3").style.color = "black";
+document.getElementById("Box3").style.fontSize = "50px";
+document.getElementById("Box3").style.background = "pink";
